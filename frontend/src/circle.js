a77df78da36bdea09cc5bc4696672f4ce8844bd6
see leng <seelengxd@gmail.com>
2023-01-15 06:29:32 +0800

    feat(circle): add gradient to path

--- frontend/src/circle.js
+++ frontend/src/circle.js
@@ -17,11 +17,22 @@ function Circle({
     const code_arr = path.filter((stations) =>
       stations.split(" ").includes(id)
     );
+    const n = path.length;
     const selected = code_arr.length > 0;
     const index = selected ? path.indexOf(code_arr[0]) : null;
     if (selected) {
       const func = () => {
-        document.getElementById(id).style.fill = "orange";
+        const a = 237;
+        const b = 93;
+        const c = 71;
+        const d = 189;
+        const e = 138;
+        const f = 230;
+        document.getElementById(id).style.fill = `rgb(${
+          a + (d - a) * ((1 + index) / (n + 1))
+        },${b + (e - b) * ((1 + index) / (n + 1))},${
+          c + (f - c) * ((1 + index) / (n + 1))
+        })`;
       };
       const timeout = setTimeout(func, (index + 1.5) * 500);
       return () => clearTimeout(timeout);
